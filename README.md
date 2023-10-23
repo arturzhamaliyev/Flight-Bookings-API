0181009b6bf0fa31d3f8f91a6197172125e7df54
art <artur.zhamaliev@gmail.com>
2023-10-23 18:38:36 +0600

    Readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Flight-Bookings-API
+
+## Description
--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -4,3 +4,4 @@ import "errors"
 
 // ErrUniqueViolation is returned when the data already exists.
 var ErrUniqueViolation = errors.New("duplicate key value violates unique constraint")
+var k
